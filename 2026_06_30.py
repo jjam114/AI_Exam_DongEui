0c7b60417e3fef14b6735c270f21da89ddc1bbad
jjam114 <126452084+jjam114@users.noreply.github.com>
2026-06-30 15:33:40 +0900

    Update 2026_06_30.py

--- 2026_06_30.py
+++ 2026_06_30.py
@@ -64,3 +64,39 @@ plt.xlabel('length')
 plt.ylabel('weight')
 plt.show()
 
+print(distances)
+
+
+
+plt.scatter(train_input[:,0], train_input[:,1])
+plt.scatter(25, 150, marker='^')
+plt.scatter(train_input[indexes, 0], train_input[indexes, 1], marker='D')
+plt.xlim((0, 1000))
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
+
+
+mean = np.mean(train_input, axis = 0)
+std = np.std(train_input, axis = 0)
+
+train_scaled = (train_input - mean) / std
+
+plt.scatter(train_scaled[:,0], train_scaled[:,1])
+plt.scatter(25, 150, marker='^')
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
+
+new = ([25, 150] - mean) / std
+plt.scatter(train_scaled[:,0], train_scaled[:,1])
+plt.scatter(new[0], new[1], marker='^')
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
+
+kn.fit(train_scaled, train_target)
+test_scaled = (test_input - mean) / std
+
+kn.score(test_scaled, test_target)
+print(kn.predict([new]))
