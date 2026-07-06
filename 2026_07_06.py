73f7f865817baae7f627268125b0932e444843e1
jjam114 <126452084+jjam114@users.noreply.github.com>
2026-07-06 16:14:31 +0900

    Update 2026_07_06.py

--- 2026_07_06.py
+++ 2026_07_06.py
@@ -27,3 +27,67 @@ kn = KNeighborsClassifier(n_neighbors=3)
 kn.fit(train_scaled, train_target)
 print(kn.score(train_scaled, train_target))
 print(kn.score(test_scaled, test_target))
+
+print(kn.classes_)
+
+
+print(kn.predict(test_scaled[:5]))
+
+import numpy as np
+proba = kn.predict_proba(test_scaled[:5])
+print(np.round(proba, decimals=4))
+
+
+distances, indexes = kn.kneighbors(test_scaled[3:4])
+print(train_target.iloc[indexes[0]])
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+z = np.arange(-5, 5, 0.1)
+phi = 1 / (1 + np.exp(-z))
+plt.plot(z, phi)
+plt.xlabel('z')
+plt.ylabel('phi')
+plt.show()
+
+
+
+char_arr = np.array(['A', 'B', 'C', 'D', 'E', 'F'])
+print(char_arr[[True, False, True, False, False, False]])
+
+
+bream_smelt_indexes = (train_target == 'Bream') | (train_target == 'Smelt')
+train_bream_smelt = train_scaled[bream_smelt_indexes]
+target_bream_smelt = train_target[bream_smelt_indexes]
+
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression()
+lr.fit(train_bream_smelt, target_bream_smelt)
+
+print(lr.predict(train_bream_smelt[:5]))
+
+print(lr.predict_proba(train_bream_smelt[:5]))
+
+
+lr = LogisticRegression(C = 20, max_iter=1000)
+lr.fit(train_scaled, train_target)
+print(lr.score(train_scaled, train_target))
+print(lr.score(test_scaled, test_target))
+
+
+print(lr.predict(test_scaled[:5]))
+
+proba = lr.predict_proba(test_scaled[:5])
+print(np.round(proba, decimals=3))
+
+
+
+decision = lr.decision_function(test_scaled[:5])
+print(np.round(decision, decimals=2))
+
+
+from scipy.special import softmax
+proba = softmax(decision, axis=1)
+print(np.round(proba, decimals=3))
